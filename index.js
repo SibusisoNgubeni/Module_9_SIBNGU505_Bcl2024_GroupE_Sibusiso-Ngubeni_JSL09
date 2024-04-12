c152eb47950d647cc613cd68cb4b6df01cccb3ca
SibusisoNgubeni <156700279+SibusisoNgubeni@users.noreply.github.com>
2024-04-12 16:01:09 +0200

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ fetch(
   .catch((err) => {
     // Use a default background image/author
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`;
+  )`;
     document.getElementById("author").textContent = `By: Dodi Achmad`;
   });
 
@@ -22,14 +22,14 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
   })
   .then((data) => {
     document.getElementById("crypto-top").innerHTML = `
-            <img src=${data.image.small} />
-            <span>${data.name}</span>
-        `;
+              <img src=${data.image.small} />
+              <span>${data.name}</span>
+          `;
     document.getElementById("crypto").innerHTML += `
-            <p>🎯: $${data.market_data.current_price.usd}</p>
-            <p>⬆️: $${data.market_data.high_24H.usd}</p>
-            <p>⬇️: $${data.market_data.low_24H.usd}</p>
-        `;
+              <p>🎯: $${data.market_data.current_price.usd}</p>
+              <p>⬆️: $${data.market_data.high_24H.usd}</p>
+              <p>⬇️: $${data.market_data.low_24H.usd}</p>
+          `;
   })
   .catch((err) => console.error(err));
 
@@ -56,10 +56,10 @@ navigator.geolocation.getCurrentPosition((position) => {
     .then((data) => {
       const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
       document.getElementById("weather").innerHTML = `
-                <img src=${iconUrl} />
-                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
-                <p class="weather-city">${data.name}</p>
-            `;
+                  <img src=${iconUrl} />
+                  <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                  <p class="weather-city">${data.name}</p>
+              `;
     })
     .catch((err) => console.error(err));
 });
